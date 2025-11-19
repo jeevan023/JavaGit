79f2cbcb49a1552d5d838a519627ce21f2969073
Devendra Kandel <jeevan164@gmail.com>
2025-11-19 12:41:59 +0545

    simple ststement updated

---
+++ question2.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class question2 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class question2
+{
+    public static void main(String[] args) {
+        System.out.println("My name is :");
+        
+        String name = "Devendra Kandel";
+        
+        System.out.println(name);
+    }
+}
---
+++ question3.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class question3 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class question3
+{
+    public static void main(String[] args)
+    {
+    int a=10;
+    int b=10;
+    System.out.println(++a);
+    System.out.println(a++);
+    }
+}
