d88b37a257ace2d8119da0ce3407be35228c9055
Devendra Kandel <jeevan164@gmail.com>
2025-11-19 12:35:33 +0545

    Initial sharing of project

---
+++ lab1.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class lab1 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class lab1
+{
+    public static void main(String[] args )
+    {
+    System.out.println("My Name is :");
+    System.out.println("Devendra Kandel");
+}
+}
+
